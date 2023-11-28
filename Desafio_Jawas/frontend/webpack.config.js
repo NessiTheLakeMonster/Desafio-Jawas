8562dd8c61eb7387abfc8c5d478ce7ff8cc5724d
Inés Mª Barrera Llerena <imbarreral020301@gmail.com>
2023-11-28 13:17:41 +0100

    Rutas arregladas

--- Desafio_Jawas/frontend/webpack.config.js
+++ Desafio_Jawas/frontend/webpack.config.js
@@ -7,45 +7,13 @@ module.exports = {
         login: './js/login.js',
         registro: './js/registro.js',
         home: './js/home.js',
-        clases: './js/utils/clases.js',
-        validaciones: './js/utils/validaciones.js',
         gestionUsuarios: './js/gestionUsuarios.js',
-        http_login: './js/http/http_login.js',
-        http_registro: './js/http/http_registro.js',
-        http_gestionUsuarios: './js/http/http_gestionUsuarios.js',
         donaciones: './js/gestion_lotes.js',
     },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
-    /* plugins: [
-        new HtmlWebpackPlugin({
-            template: './html/index.html',
-            filename: 'index.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: './html/login.html',
-            filename: 'login.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: './html/registro.html',
-            filename: 'registro.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: './html/home.html',
-            filename: 'home.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: './html/gestionUsuarios.html',
-            filename: 'gestionUsuarios.html',
-        }
-        new HtmlWebpackPlugin({
-            template: './html/donacion.html',
-            filename: 'donacion.html',
-        }), 
-    ] */
-
     devServer: {
         static: path.resolve(__dirname, '../'),
         port: 8090,
