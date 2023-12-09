0bb35a2bf950e6840a91dd12f7ae58e822cd66ba
Jaime <jaime.ornu@gmail.com>
2023-12-09 16:27:49 +0100

    Cambios varios(tabla nom,local a sesion,tabla bien

--- Desafio_Jawas/frontend/js/gestion_lotes.js
+++ Desafio_Jawas/frontend/js/gestion_lotes.js
@@ -3,11 +3,13 @@
  */
 
 import { mostrarLotes, mostrarLote, cancelarLote } from './http/http_gestionLotes.js'
+import { getUsuarioById } from './http/http_gestionUsuarios.js'
 
-let idUsuario = sessionStorage.getItem('idUsuario')
-console.log(idUsuario)
-let latitud = localStorage.getItem('latitud')
-let longitud = localStorage.getItem('longitud')
+let idUsuarioGuardado = sessionStorage.getItem('userId')
+let idUsuario = JSON.parse(idUsuarioGuardado)
+
+let latitud = sessionStorage.getItem('latitud')
+let longitud = sessionStorage.getItem('longitud')
 
 let mandarLote = document.getElementById('mandarLote')
 let tablaLotes = document.getElementById('tablaLotes')
@@ -32,7 +34,7 @@ export function move() {
 export function cabeceraTabla(data) {
     let cabecera = document.createElement('tr')
     let columnas = ['', 'Nº LOTE', 'USUARIO', 'LUGAR RECOGIDA', 'ENTREGADO', 'CANCELADO']
-    
+
     columnas.forEach(col => {
         let th = document.createElement('th')
         th.textContent = col
@@ -42,13 +44,13 @@ export function cabeceraTabla(data) {
     tablaLotes.appendChild(cabecera);
 }
 
-export function llenarTablaLotes(data) {
+export function llenarTablaLotes(data, dataUser) {
     return data.map(lote => `
                 <tr>
                     <td><input class="checkbox-lote" type="checkbox" name"lote" value="${lote.id}"</td>
                     <td>${lote.id}</td>
-                    <td>${lote.idUsuario}</td>
-                    <td>${latitud + ', ' + longitud}</td>
+                    <td>${dataUser.nombre}</td>
+                    <td>${lote.lugar_recogida}</td>
                     <td>${lote.entregado}</td>
                     <td>${lote.cancelado}</td>
                 </tr>
@@ -56,45 +58,35 @@ export function llenarTablaLotes(data) {
 }
 
 export function _Init() {
-    mostrarLotes(idUsuario).then(data => {
-        cabeceraTabla(data)
-        tablaLotes.innerHTML += llenarTablaLotes(data)
-    })
-    .catch(error => console.error('Error:', error))
+    getUsuarioById(idUsuario).then(dataUser => {
+        mostrarLotes(idUsuario).then(data => {
+            cabeceraTabla(data)
+            tablaLotes.innerHTML += llenarTablaLotes(data, dataUser)
 
-    let checkboxes = document.querySelectorAll('.checkbox-lote')
-    
-    checkboxes.forEach(checkbox => {
-        checkbox.addEventListener('change', function() {
-            if (this.checked) {
+            let checkboxes = document.querySelectorAll('.checkbox-lote');
 
-                checkboxes.forEach(otraCheckbox => {
-                    if (otraCheckbox !== checkbox) {
-                        otraCheckbox.checked = false;
+            checkboxes.forEach(checkbox => {
+                checkbox.addEventListener('change', function () {
+                    if (this.checked) {
+
+                        checkboxes.forEach(otherCheckbox => {
+                            if (otherCheckbox !== checkbox) {
+                                otherCheckbox.checked = false;
+                            }
+                        });
+                        // Si el checkbox está seleccionado, guardar el ID en el localStorage
+                        localStorage.setItem('loteIdGestion', this.value);
+                    } else {
+                        // Si el checkbox no está seleccionado, eliminar el ID del localStorage
+                        localStorage.removeItem('loteIdGestion');
                     }
                 });
-                // Si el checkbox está seleccionado, guardar el ID en el localStorage
-                localStorage.setItem('usuarioId', this.value);
-            } else {
-                // Si el checkbox no está seleccionado, eliminar el ID del localStorage
-                localStorage.removeItem('usuarioId');
-            }
+            });
         })
+            .catch(error => console.error('Error:', error))
     })
+        .catch(error => console.error('Error:', error))
 
-    
+
 }
 _Init()
-
-    // mandarLote.addEventListener('click', async function () {
-    //     let data = await mostrarLotes()
-
-    //     tablaLotes.innerHTML = ''
-
-    //     if (data && data.id !== undefined) {
-    //         data = Array.isArray(data) ? data : [data]
-
-    //         tablaLotes.innerHTML += llenarTablaLotes(data)
-    //     } 
-    //     console.log('CLICASTE')
-    // })
